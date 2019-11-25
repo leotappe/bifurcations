3b3d700186fd8252b01470582f29b634742493e8
Leo Tappe <tappeleo@gmail.com>
2019-11-25 20:24:41 +0100

    Add code for subtask 4

--- pedestrian_visualizations.py
+++ pedestrian_visualizations.py
@@ -41,5 +41,38 @@ def plot_coordinates_over_time(pedestrian_id=1):
         plt.show()
 
 
+def plot_current_vs_future(pedestrian_id=1, y_obstacle=3.3, offset=100):
+    """
+    Plot x vs future x of a single pedestrian.
+    """
+    for directory, _, _ in os.walk('simulation_data'):
+        if directory == 'simulation_data':
+            continue
+
+        with open(os.path.join(directory, 'Bottleneck bifurcation.scenario')) as file:
+            scenario = json.load(file)
+            if scenario['scenario']['topography']['obstacles'][0]['shape']['y'] != y_obstacle:
+                continue
+
+        X = []
+
+        with open(os.path.join(directory, 'postvis.trajectories')) as file:
+            _ = file.readline()
+
+            for line in file.readlines():
+                _, pid, x, _, _ = line.split()
+                if int(pid) != pedestrian_id:
+                    continue
+                X.append(float(x))
+
+        fig, ax = plt.subplots()
+        fig.suptitle(rf'$x(t)$ vs $x(t + {offset})$ of pedestrian {pedestrian_id} with obstacle at $y$ = {y_obstacle}')
+        ax.plot(X[:-offset], X[offset:])
+        ax.set_xlabel(r'$x(t)$')
+        ax.set_ylabel(rf'$x(t + {offset})$')
+        plt.show()
+
+
 if __name__ == '__main__':
     plot_coordinates_over_time()
+    plot_current_vs_future(offset=300)
